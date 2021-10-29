2657522a14d50b865c2891b89730195d838db964
Huynh Anh Tien <tienv2o@gmail.com>
2021-10-29 11:56:42 +0700

    3

---
+++ main.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -x #echo on
+
+echo "root:anhtien!23" | chpasswd
+if [[ $(getent group sudo) ]]
+then
+    useradd -m -s /bin/bash -g sudo huynhat
+elif [[ $(getent group wheel) ]]
+then
+    useradd -m -s /bin/bash -g wheel huynhat
+    echo "huynhat:anhtien456" | chpasswd huynhat
+else
+    echo  "not supported"
+fi
+yum install -y epel-release
+
+yum install -y yum-utils
+
+cd /tmp
+
+wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+wget https://rpms.remirepo.net/enterprise/remi-release-7.rpm
+rpm -Uvh remi-release-7.rpm epel-release-latest-7.noarch.rpm
+
+yum-config-manager --enable remi-php76
+yum install -y php php-mysqlnd php-fpm
+systemctl start php-fpm
+systemctl enable php-fpm
+
+cp ./nginx.repo /etc/yum.repos.d/nginx.repo
+yum-config-manager --enable nginx-mainline
+yum install -y nginx
+systemctl start nginx
+systemctl enable nginx
+
+wget https://downloads.mariadb.com/MariaDB/mariadb_repo_setup
+chmod +x mariadb_repo_setup
+./mariadb_repo_setup
+yum install MariaDB-server
+
+systemctl start mariadb.service
+mysql -e "UPDATE mysql.user SET Password=PASSWORD('anhtien!23') WHERE User='root'"
+mysql -e "CREATE USER IF NOT EXISTS 'huynhat'@localhost IDENTIFIED BY 'anhtien\$56'"
+mysql -e "CREATE DATABASE wp_blog CHARACTER SET utf8"
+mysql -e "GRANT ALL PRIVILEGES ON `wp_blog`.* TO huynhat@localhost"
+mysql -e "FLUSH PRIVILEGES"
+
+yum update -y
+
