0f8a14fa803215d7e0dd21a4e09f203470222dc1
Huynh Anh Tien <tienv2o@gmail.com>
2021-10-29 11:30:41 +0700

    2

--- epel_remi.sh
+++ epel_remi.sh
@@ -1,13 +1,14 @@
 set -x #echo on
 
 yum install -y epel-release
-yum install -y remi-release.noarch
+
 yum install -y yum-utils
-# cd /tmp
-yum install -y yum-utils
-# wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-# wget https://rpms.remirepo.net/enterprise/remi-release-7.rpm
-# rpm -Uvh remi-release-7.rpm epel-release-latest-7.noarch.rpm
-# yum install -y yum-utils
-# yum-config-manager --enable remi-php76
-# yum update -y
+
+cd /tmp
+
+wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+wget https://rpms.remirepo.net/enterprise/remi-release-7.rpm
+rpm -Uvh remi-release-7.rpm epel-release-latest-7.noarch.rpm
+
+yum-config-manager --enable remi-php76
+yum update -y
