2bcfcb50fca25218e194eb807700fc0352d08231
Huynh Anh Tien <tienv2o@gmail.com>
2021-10-29 11:21:06 +0700

    11:21

--- checking.sh
+++
@@ -1,9 +0,0 @@
-if [[ $(getent group sudo) ]]
-then
-    echo "Sudo"
-elif [[ $(getent group wheel) ]]
-then
-    echo "Wheel"
-else
-    echo  "not supported"
-fi
---
+++ user.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "root:anhtien!23" | chpasswd
+if [[ $(getent group sudo) ]]
+then
+    useradd -m -s /bin/bash -g sudo huynhat
+elif [[ $(getent group wheel) ]]
+then
+    useradd -m -s /bin/bash -g wheel huynhat
+    echo "huynhat:anhtien456" | chpasswd huynhat
+else
+    echo  "not supported"
+fi
